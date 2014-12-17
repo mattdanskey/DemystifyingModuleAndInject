1593b74e9be13acb6441c84e720d3b2fc6103ce8
mdanskey <mdanskey@gmail.com>
2014-12-16 19:30:03 -0800

    add the code written live

--- src/app/myController.spec.js
+++ src/app/myController.spec.js
@@ -1,43 +1,36 @@
 describe('Controller: myController', function () {
-  var myController,
-    $controller,
-    myFactory,
-    scope;
+  var controller;
+  var $controller;
+  var myFactory;
 
   beforeEach(function () {
-    module('myApp');
-    inject(function ($injector, $rootScope, _$controller_) {
-      $controller = _$controller_; // magic syntax so I can have a reference of the same name as what I'm trying to get via injector. Angular ignores the underscores when looking up the dependency.
-      myFactory = $injector.get('myFactory'); //alternative to magic syntax (my preferred)
-      scope = $rootScope.$new();
-      myController = $controller('myController', {
-        scope: scope
+    module('myApp', function ($provide) {
+      $provide.value('myFactory', {
+        getGreetingsAsync: function () {
+          return {
+            then: function () {}
+          };
+        }
       });
+    });
+    inject(function ($injector) {
+      $controller = $injector.get('$controller');
+      myFactory = $injector.get('myFactory');
+      controller = $controller('myController');
     });
   });
 
   describe('initial state', function () {
-    it('shows hello', function () {
-      expect(myController.showHello).toBe(true);
+    it('has showHello as true', function () {
+      expect(controller.showHello).toBe(true);
     });
-    it('exposes doSomething', function () {
-      expect(myController.doSomething).toEqual(myFactory.doSomething);
-    });
-    it('gets the greetings from myFactory', function () {
+
+    it('calls myFactory\'s getGreetingsAsync', function () {
       spyOn(myFactory, 'getGreetingsAsync').and.callThrough();
 
-      //instantiating controller again since getGreetingsAsync has already been
-      //called! Which means the spy we just implemented hasn't had a chance to be called yet.
       $controller('myController');
 
       expect(myFactory.getGreetingsAsync).toHaveBeenCalled();
-    });
-  });
-
-  describe('greetings', function () {
-    it('gets populated', function () {
-      scope.$digest();
-      expect(myController.greetings).toEqual(['Hello!', 'Hi!', 'Welcome!']);
     });
   });
 });
